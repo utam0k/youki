bffdd370fdcf7f94edd072d52aca2f81936397f7
utam0k <k0ma@utam0k.jp>
2021-06-05 18:45:26 +0900

    up.

--- src/process/parent.rs
+++ src/process/parent.rs
@@ -14,8 +14,8 @@ pub struct ParentProcess {
 impl ParentProcess {
     pub fn new() -> Result<(Self, Sender)> {
         let (sender, mut receiver) = pipe::new()?;
-        sender.set_nonblocking(true)?;
-        receiver.set_nonblocking(true)?;
+        // sender.set_nonblocking(false)?;
+        // receiver.set_nonblocking(false)?;
         let poll = Poll::new()?;
         poll.registry()
             .register(&mut receiver, PARENT, Interest::READABLE)?;
@@ -24,15 +24,29 @@ impl ParentProcess {
 
     pub fn wait_for_child_ready(&mut self) -> Result<i32> {
         let mut events = Events::with_capacity(128);
+        // loop {
         self.poll.poll(&mut events, Some(Duration::from_secs(2)))?;
         for event in events.iter() {
             if let PARENT = event.token() {
-                let mut buf = [0; 16];
-                self.receiver.read_exact(&mut buf)?;
+                // let mut buf = [0; 20];
+                eprintln!("buffer start");
+                let mut buf = vec![];
+                eprintln!("buffer ok");
+                // self.receiver.read_to_end(&mut buf)?;
+                if let Err(e) = self.receiver.read_to_end(&mut buf) {
+                    if e.kind() == std::io::ErrorKind::WouldBlock {
+                        // let message = String::from_utf8_lossy(&buf).to_string();
+                        // eprintln!("retry because of WouldBlock: {}", message);
+                        // bail!("aa")
+                    } else {
+                        bail!("{:?}", e);
+                    }
+                }
+
                 log::debug!("receive a message from child: {:?}", buf);
                 let message = String::from_utf8_lossy(&buf).to_string();
                 // let err_msg = format!("receive unexpected message {:?} in parent process", &message);
-                let err_msg = format!("receive unexpected message in parent process",);
+                let err_msg = "receive unexpected message in parent process".to_string();
                 let mut a = message.split("=");
                 let sign = a.next().expect(&err_msg);
 
@@ -53,6 +67,7 @@ impl ParentProcess {
                 unreachable!()
             }
         }
+        // }
         bail!("unexpected message.")
     }
 }
